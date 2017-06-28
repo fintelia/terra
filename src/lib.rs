117c4e54d9cd4f22ce85a58a374fb7ed209ec9a8
Jonathan Behrens <fintelia@gmail.com>
2017-06-28 00:44:13 -0400

    Added code to download and parse DEMs from the USGS

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,16 @@ extern crate gfx_core;
 extern crate shader_version;
 extern crate rand;
 
+#[cfg(feature = "download")]
+extern crate byteorder;
+#[cfg(feature = "download")]
+extern crate curl;
+#[cfg(feature = "download")]
+extern crate zip;
+
 pub mod terrain;
+#[cfg(feature = "download")]
+pub mod offline;
 
 #[cfg(test)]
 mod tests {
