8421060c170a0c3692f0fa295b584f1adee07933
Jonathan Behrens <fintelia@gmail.com>
2020-12-24 15:45:30 -0500

    Remove unused coordinate system code to reduce dependencies

--- src/coordinates.rs
+++ src/coordinates.rs
@@ -18,29 +18,12 @@
 //! *cspace* - Restricted to points on the unit cube, projected from polar.
 
 use cgmath::{InnerSpace, Vector3};
-use coord_transforms::geo;
-use coord_transforms::structs::geo_ellipsoid::geo_ellipsoid as GeoEllipsoid;
-use coord_transforms::structs::geo_ellipsoid::{WGS84_SEMI_MAJOR_AXIS_METERS, WGS84_FLATTENING};
-use nalgebra as na;
 
-lazy_static! {
-    static ref ELLIPSOID: GeoEllipsoid =
-        GeoEllipsoid::new(WGS84_SEMI_MAJOR_AXIS_METERS, WGS84_FLATTENING);
-}
+const WGS84_INV_FLATTENING: f64 = 298.257223563;
+const WGS84_SEMI_MAJOR_AXIS_METERS: f64 = 6378137.0;
+const WSG84_SEMI_MINOR_AXIS_METERS: f64 = WGS84_SEMI_MAJOR_AXIS_METERS * (1.0 - 1.0 / WGS84_INV_FLATTENING);
 
 pub const PLANET_RADIUS: f64 = 6371000.0;
-
-
-#[allow(unused)]
-pub fn ecef_to_lla(ecef: Vector3<f64>) -> Vector3<f64> {
-    let lla = geo::ecef2lla(&na::Vector3::new(ecef.x, ecef.y, ecef.z), &ELLIPSOID);
-    Vector3::new(lla.x, lla.y, lla.z)
-}
-#[allow(unused)]
-pub fn lla_to_ecef(lla: Vector3<f64>) -> Vector3<f64> {
-    let ecef = geo::lla2ecef(&na::Vector3::new(lla.x, lla.y, lla.z), &ELLIPSOID);
-    Vector3::new(ecef.x, ecef.y, ecef.z)
-}
 
 #[inline]
 #[allow(unused)]
@@ -62,9 +45,9 @@ pub fn polar_to_ecef(lla: Vector3<f64>) -> Vector3<f64> {
 #[allow(unused)]
 pub fn ecef_to_warped(ecef: Vector3<f64>) -> Vector3<f64> {
     Vector3::new(
-        ecef.x * PLANET_RADIUS / ELLIPSOID.get_semi_major_axis(),
-        ecef.y * PLANET_RADIUS / ELLIPSOID.get_semi_major_axis(),
-        ecef.z * PLANET_RADIUS / ELLIPSOID.get_semi_minor_axis(),
+        ecef.x * PLANET_RADIUS / WGS84_SEMI_MAJOR_AXIS_METERS,
+        ecef.y * PLANET_RADIUS / WGS84_SEMI_MAJOR_AXIS_METERS,
+        ecef.z * PLANET_RADIUS / WSG84_SEMI_MINOR_AXIS_METERS,
     )
 }
 
@@ -72,9 +55,9 @@ pub fn ecef_to_warped(ecef: Vector3<f64>) -> Vector3<f64> {
 #[allow(unused)]
 pub fn warped_to_ecef(warped: Vector3<f64>) -> Vector3<f64> {
     Vector3::new(
-        warped.x * ELLIPSOID.get_semi_major_axis() / PLANET_RADIUS,
-        warped.y * ELLIPSOID.get_semi_major_axis() / PLANET_RADIUS,
-        warped.z * ELLIPSOID.get_semi_minor_axis() / PLANET_RADIUS,
+        warped.x * WGS84_SEMI_MAJOR_AXIS_METERS / PLANET_RADIUS,
+        warped.y * WGS84_SEMI_MAJOR_AXIS_METERS / PLANET_RADIUS,
+        warped.z * WSG84_SEMI_MINOR_AXIS_METERS / PLANET_RADIUS,
     )
 }
 
@@ -106,51 +89,4 @@ pub fn cspace_to_polar(position: Vector3<f64>) -> Vector3<f64> {
     let latitude = f64::asin(p.z);
     let longitude = f64::atan2(p.y, p.x);
     Vector3::new(latitude, longitude, 0.0)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use cgmath::assert_relative_eq;
-
-    fn vec3_na2cgmath(v: na::Vector3<f64>) -> Vector3<f64> {
-        Vector3::new(v.x, v.y, v.z)
-    }
-
-    #[test]
-    fn test_lla_to_ecef() {
-        assert_eq!(
-            lla_to_ecef(Vector3::new(0.2345, -0.637, 10.0)),
-            vec3_na2cgmath(geo::lla2ecef(&na::Vector3::new(0.2345, -0.637, 10.0), &ELLIPSOID)),
-        );
-        assert_relative_eq!(
-            lla_to_ecef(Vector3::new(19.0f64.to_radians(), -34.0f64.to_radians(), 10.0,)),
-            Vector3::new(5001415.53283897, -3373497.37316789, 2063352.71871789),
-            epsilon = 0.001
-        );
-    }
-
-    #[test]
-    fn test_lla_ecef_lla() {
-        let roundtrip =
-            |p: Vector3<f64>| -> Vector3<f64> { ecef_to_lla(lla_to_ecef(p)) };
-
-        let a = Vector3::new(0.0, 0.0, 50.0);
-        let b = Vector3::new(40.0f64.to_radians(), 50.0f64.to_radians(), 100.0);
-
-        assert_relative_eq!(a, roundtrip(a), epsilon = 0.1);
-        assert_relative_eq!(b, roundtrip(b), epsilon = 0.1);
-    }
-
-    #[test]
-    fn test_ecef_lla_ecef() {
-        let roundtrip =
-            |p: Vector3<f64>| -> Vector3<f64> { lla_to_ecef(ecef_to_lla(p)) };
-
-        let a = Vector3::new(-5280434.995591136, 342.0201433256688, 4429584.375659218);
-        let b = Vector3::new(-4880469.147111009, 0.0, 4095199.8613129416);
-
-        assert_relative_eq!(a, roundtrip(a), epsilon = 0.1);
-        assert_relative_eq!(b, roundtrip(b), epsilon = 0.1);
-    }
 }
