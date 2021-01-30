b8f54722f68b8970e1485baf86f43f62286a99b6
Jonathan Behrens <fintelia@gmail.com>
2021-01-30 18:35:40 -0500

    Framebuffer attachement should be configured with operation store=true
    Fixes #13

--- src/lib.rs
+++ src/lib.rs
@@ -447,7 +447,7 @@ impl Terrain {
                     resolve_target: None,
                     ops: wgpu::Operations {
                         load: wgpu::LoadOp::Clear(wgpu::Color { r: 0.0, g: 0.0, b: 0.0, a: 1.0 }),
-                        store: false,
+                        store: true,
                     },
                 }],
                 depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachmentDescriptor {
